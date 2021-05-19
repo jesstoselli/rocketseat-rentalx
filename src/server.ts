69990f318ab2d1de9ec3aabacb60eb008acaf5c7
Jessyca Toselli <jfamedeiros@gmail.com>
2021-05-19 17:57:39 -0300

    Base app

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+// app.get("/", (request, response) => {
+//     return response.json({ message: "Hello World" });
+// });
+
+app.listen(3333, () => console.log("Server running. 🚀"));
